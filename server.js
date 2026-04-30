f80c4fe221979a00e8fa3f65bfedc764d7e507a9
fk aura <fkfunds2010@gmail.com>
2026-04-30 16:47:39 +0100

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { Schema, MapSchema } = require("@colyseus/schema");
 const express = require("express");
 const cors = require("cors");
 const { playground } = require("@colyseus/playground");
-const { matchMaker } = require("@colyseus/matchmaking");
 
 // ---------- Schemas ----------
 class PlayerState extends Schema {
@@ -321,7 +320,6 @@ app.get("/", (_, res) => res.send("Football server is running ✅"));
 app.get("/health", (_, res) => res.send("OK"));
 
 app.use("/playground", playground());
-app.use("/matchmake", matchMaker.express());
 
 const port = process.env.PORT || 2567;
 const httpServer = app.listen(port, () => {
